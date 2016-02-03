83ba75bda5148d84b232ec53eab974bc55b37df8
Martyna W <Martyna W>
2016-02-03 20:55:30 +0100

    Drukowanie planszy

--- Plansza.java
+++ Plansza.java
@@ -1,8 +1,20 @@
+import java.io.*;
 
 public class Plansza{
-  private int[] plansza;
+  private int[][] plansza;
+  PrintWriter wyj = new PrintWriter(System.out, true);
+  
   public Plansza(){
-    plansza = {{0,0,0},{0,0,0},{0,0,0}};
+    plansza = new int[][]{{0,0,0},{0,0,0},{0,0,0}};
+  }
+  
+  public void drukuj(){
+	  for(int i=0; i<plansza.length; i++){
+		  for(int j=0; j<plansza[0].length; j++){
+			  wyj.printf("%3d%s",plansza[i][j]," ");
+		  }
+		  wyj.println();
+	  }
   }
   
 }

922c516ea3ffcaa519d115c1497e65236953b22e
gchilczuk <chilczukg@gmail.com>
2016-02-08 23:37:54 +0100

    metoda czyKoniecGry i pomocnicze

--- src/Plansza.java
+++ src/Plansza.java
@@ -1,5 +1,6 @@
 import java.io.*;
 
+
 /**
  * Reprezentuje planszę do gry w kółko i  krzyżyk
  */
@@ -32,9 +33,60 @@ public class Plansza {
      */
     public void ruchGracza(int kolumna, int wiersz, int gracz) {
         if ((kolumna < 0) || (kolumna > 2) || (wiersz < 0) || (wiersz > 2)) {
-            throw NullPointerException("Nieprawidłowy adres komórki");
+            throw new NullPointerException("Nieprawidłowy adres komórki");
         }
         this.plansza[wiersz][kolumna] = gracz;
+    }
+
+    /**
+     * Sprawdza czy gra została zakończona (nieważne czy remisem czy wygraną)
+     * @return true jeżeli gra została zakończona remisem LUB wygraną; else: false
+     */
+    public boolean czyKoniecGry(){
+        boolean czy_wygrana = false;
+        boolean czy_remis = true;
+        if (    Math.abs(sumaWiersza(0)) == 3 ||
+                Math.abs(sumaWiersza(1)) == 3 ||
+                Math.abs(sumaWiersza(2)) == 3 ||
+                Math.abs(sumaKolumny(0)) == 3 ||
+                Math.abs(sumaKolumny(1)) == 3 ||
+                Math.abs(sumaKolumny(2)) == 3   )
+            czy_wygrana = true;
+        if (!czy_wygrana){
+            for (int w = 0; w < 3; w++){
+                for (int k = 0; k < 3; k++){
+                    if (plansza[w][k] == 0){
+                        czy_remis = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return czy_wygrana || czy_remis;
+
+    }
+
+    /**
+     * Metoda pomocnicza
+     *
+     * Sumuje wierwsz planszy
+     * @param i numer wiersza, który ma zostać zsumowany
+     * @return suma elementów w wierszu
+     */
+    private int sumaWiersza(int i){
+        int s = 0;
+        for (int j = 0; j < 3; j++)
+            s += this.plansza[i][j];
+        return s;
+    }
+
+    /**
+     * Sumuje kolumnę planszy
+     * @param i kolumna, która ma zostać zsumowana
+     * @return suma elementów kolumny
+     */
+    private int sumaKolumny(int i){
+        return this.plansza[0][i] + this.plansza[1][i] + this.plansza[2][i];
     }
 
 

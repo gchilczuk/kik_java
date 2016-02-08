096edf33f099a22f9e74c2a84ab2a4b8fe0f61af
Martyna W <Martyna W>
2016-02-08 23:49:39 +0100

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 /**
- * Klasa testujÄ…ca
+ * Klasa testuj¹ca
  */
 public class Main {
     public static void main(String[] args){
         // TODO Auto-generated method stub
         Plansza p = new Plansza();
+        p.drukuj();
 
 
     }

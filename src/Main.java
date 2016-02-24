e2c254e102220921055702178023d0f08b97104a
marwaw <martyna.waw96@gmail.com>
2016-02-24 22:23:13 +0100

    Drobne zmiany

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import javax.swing.JFrame;
+
 /**
  * Klasa testuj�ca
  */
 public class Main {
     public static void main(String[] args){
-        TUI tui = new TUI();
-        tui.start();
+        //TUI tui = new TUI();
+        //tui.start();
+        GUI gui = new GUI();
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setVisible(true);
     }
 }
